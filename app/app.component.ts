cb45f31811968207f36eb6568714bf72e3ba8d1b
namelos <namelos@outlook.com>
2016-04-10 23:41:04 +0800

    start

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,11 @@
 import {Component} from 'angular2/core';
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+  selector: 'my-app',
+  template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
 })
-export class AppComponent { }
+
+export class AppComponent {
+  title = 'Tour of Heroes'
+  hero = 'Windstorm'
+}
